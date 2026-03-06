de296f72d5e604983b5a49d515bd8bcbd73dad33
Abhishek Patel <ap975498@gmail.com>
2026-03-06 18:40:47 +0530

    export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone', // Enable standalone output for Docker
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,19 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
+    unoptimized: true,
   },
 };
 
